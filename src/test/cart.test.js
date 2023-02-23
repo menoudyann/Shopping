e91d39702fe163c12209c5ed427f5fb2e42faefc
menoudyann <Yann.MENOUD@cpnv.ch>
2023-02-23 10:55:31 +0100

    fix merge conflicts from upstream

--- src/test/cart.test.js
+++ src/test/cart.test.js
@@ -8,33 +8,10 @@
  */
 
 let Cart = require("../Cart/Cart.js");
-=======
- * @version   23-02-2023 - update for first eval
- */
 
-let Cart = require('../Cart/Cart.js');
->>>>>>> b3d23fe5353afbfdadb2f60f831d03ff4f1311d8
 const CartItem = require("../CartItem/CartItem.js");
 const EmptyCartException = require("../Cart/EmptyCartException.js");
 const UpdateCartException = require("../Cart/UpdateCartException.js");
-
-<<<<<<< HEAD
-test("items_NominalCase_GetItems", () => {
-  //given
-  let cartItem1 = new CartItem(1, "Iphone 27", 1, 10);
-  let cartItem2 = new CartItem(2, "Iphone 28", 2, 20);
-  let expectedItems = [cartItem1, cartItem2];
-  let actualItems = null;
-  let cart = new Cart(expectedItems);
-
-  //when
-  actualItems = cart.items;
-
-  //then
-  for (let i = 0; i <= expectedItems.length; i++) {
-    expect(expectedItems[i]).toEqual(actualItems[i]);
-  }
-});
 
 test("items_EmptyCart_ThrowException", () => {
   //given
@@ -45,21 +22,6 @@ test("items_EmptyCart_ThrowException", () => {
 
   //then
   expect(() => cart.items).toThrow(EmptyCartException);
-});
-
-test("total_NominalCase_GetsSum", () => {
-  //given
-  let cartItem1 = new CartItem(1, "Iphone 27", 1, 10);
-  let cartItem2 = new CartItem(2, "Iphone 28", 2, 20);
-  let items = [cartItem1, cartItem2];
-  let cart = new Cart(items);
-  let totalPriceExpected = 50;
-
-  //when
-  //Event triggered by th assertion
-
-  //then
-  expect(totalPriceExpected).toEqual(cart.total);
 });
 
 test("total_EmptyCart_ThrowException", () => {
@@ -73,51 +35,6 @@ test("total_EmptyCart_ThrowException", () => {
   expect(() => cart.total).toThrow(EmptyCartException);
 });
 
-test("count_OnlySingleQuantityProduct_GetsNumberOfItems", () => {
-  //given
-  let cartItem1 = new CartItem(1, "Iphone 27", 1, 10);
-  let cartItem2 = new CartItem(2, "Iphone 28", 1, 20);
-  let items = [cartItem1, cartItem2];
-  let cart = new Cart(items);
-  let countExpected = 2;
-
-  //when
-  //Event triggered by th assertion
-
-  //then
-  expect(countExpected).toEqual(cart.count());
-});
-
-test("count_MixSingleAndMultipleQuantityProduct_GetsNumberOfItems", () => {
-  //given
-  let cartItem1 = new CartItem(1, "Iphone 27", 1, 10);
-  let cartItem2 = new CartItem(2, "Iphone 28", 2, 20);
-  let items = [cartItem1, cartItem2];
-  let cart = new Cart(items);
-  let countExpected = 3;
-
-  //when
-  //Event triggered by th assertion
-
-  //then
-  expect(countExpected).toEqual(cart.count());
-});
-
-test("count_MixSingleAndMultipleQuantityProductDistinct_GetsNumberOfItems", () => {
-  //given
-  let cartItem1 = new CartItem(1, "Iphone 27", 1, 10);
-  let cartItem2 = new CartItem(2, "Iphone 28", 2, 20);
-  let items = [cartItem1, cartItem2];
-  let cart = new Cart(items);
-  let countExpected = 2;
-
-  //when
-  //Event triggered by th assertion
-
-  //then
-  expect(countExpected).toEqual(cart.count(true));
-});
-
 test("count_EmptyCart_ThrowException", () => {
   //given
   let cart = new Cart(null);
@@ -127,20 +44,6 @@ test("count_EmptyCart_ThrowException", () => {
 
   //then
   expect(() => cart.count()).toThrow(EmptyCartException);
-});
-
-test("add_EmptyCartAddFirstSingleCartItem_GetsUpdatedNumberOfItems", () => {
-  //given
-  let cart = new Cart(null);
-  let expectedTotalPrice = 10;
-  let cartItem1 = new CartItem(1, "Iphone 27", 1, expectedTotalPrice);
-  let items = [cartItem1];
-
-  //when
-  cart.add(items);
-
-  //then
-  expect(expectedTotalPrice).toEqual(cart.total);
 });
 
 test("add_EmptyCartEmptyItemsToAdd_ThrowException", () => {
@@ -154,7 +57,7 @@ test("add_EmptyCartEmptyItemsToAdd_ThrowException", () => {
   //then
   //Exception is thrown
 });
-=======
+
 test('items_NominalCase_GetItems', () => {
     //given
     let cartItem1 = new CartItem(1,"Iphone 27", 1,10);
@@ -173,17 +76,6 @@ test('items_NominalCase_GetItems', () => {
     }
 })
 
-test('items_EmptyCart_ThrowException', () => {
-    //given
-    let cart = new Cart(null);
-
-    //when
-    //Event triggered by th assertion
-
-    //then
-    expect(() => cart.items).toThrow(EmptyCartException);
-})
-
 test('total_NominalCase_GetsSum', () => {
     //given
     let cartItem1 = new CartItem(1,"Iphone 27",1,10);
@@ -197,17 +89,6 @@ test('total_NominalCase_GetsSum', () => {
 
     //then
     expect(cart.total).toEqual(totalPriceExpected);
-})
-
-test('total_EmptyCart_ThrowException', () => {
-    //given
-    let cart = new Cart(null);
-
-    //when
-    //Event triggered by th assertion
-
-    //then
-    expect(() => cart.total).toThrow(EmptyCartException);
 })
 
 test('count_OnlySingleQuantityProduct_GetsNumberOfItems', () => {
@@ -255,17 +136,6 @@ test('count_MixSingleAndMultipleQuantityProductDistinct_GetsNumberOfItems', () =
     expect(cart.count(true)).toEqual(countExpected);
 })
 
-test('count_EmptyCart_ThrowException', () => {
-    //given
-    let cart = new Cart(null);
-
-    //when
-    //Event triggered by th assertion
-
-    //then
-    expect(() => cart.count()).toThrow(EmptyCartException);
-})
-
 test('add_EmptyCartAddFirstSingleCartItem_GetsUpdatedNumberOfItems', () => {
     //given
     let cart = new Cart(null);
@@ -279,16 +149,3 @@ test('add_EmptyCartAddFirstSingleCartItem_GetsUpdatedNumberOfItems', () => {
     //then
     expect(cart.total).toEqual(cart.total);
 })
-
-test('add_EmptyCartEmptyItemsToAdd_ThrowException', () => {
-    //given
-    let cart = new Cart(null);
-    let items = null;
-
-    //when
-    expect(() => cart.add(items)).toThrow(UpdateCartException);
-
-    //then
-    //Exception is thrown
-})
->>>>>>> b3d23fe5353afbfdadb2f60f831d03ff4f1311d8
